b027467554213e6cf98d89ed0d617bda3d78f130
carlo112 <carlo.zoboli@yahoo.it>
2014-04-16 10:39:59 +0100

    newest version

--- code.ino
+++ code.ino
@@ -7,17 +7,20 @@
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
 #include "EmonLib.h"                   // Include Emon Library
-EnergyMonitor emon1, emon2;
+EnergyMonitor emon1,emon2;
 boolean alarm=true;
 int inputPin=8;
 #define REDLITE 3
 #define BLUELITE 5
 #define ONE_WIRE_BUS 7
 
+// MAC address for your Ethernet shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
+// Your Xively key to let you upload data
 char xivelyKey[] = "w8QGOAECPQVc6YSTO2NLRqbBQKTeqnFcorxH1rbrJM8qy1CS";
 
+// Analog pin which we're monitoring (0 and 1 are used by the Ethernet shield)
 int sensorPin1 = 7;
 
 int sensorPin2 = A0;
@@ -25,11 +28,11 @@ int sensorPin2 = A0;
 int sensorPin3 = A1;
 
 // Define the strings for our datastream IDs
-char sensorId1[] = "Lasertemperature";
-char sensorId2[] = "Lasercurrent";
-char sensorId3[] = "Biglaser";
-String stringId1("laser_power");
-String stringId2("big_laser");
+char sensorId1[] = "Laserstemperature";
+char sensorId2[] = "Smallasercurrent";
+char sensorId3[] = "Biglasercurrent";
+String stringId1("smallaser");
+String stringId2("biglaser");
 
 XivelyDatastream datastreams[] = {
  XivelyDatastream(sensorId1, strlen(sensorId1), DATASTREAM_FLOAT),
@@ -38,7 +41,7 @@ XivelyDatastream datastreams[] = {
  XivelyDatastream(stringId1, DATASTREAM_STRING),
  XivelyDatastream(stringId2, DATASTREAM_STRING),
 };
-XivelyFeed feed(310547398, datastreams, 5);
+XivelyFeed feed(310547398, datastreams, 5 /* number of datastreams */);
 
 EthernetClient client;
 XivelyClient xivelyclient(client);
@@ -50,29 +53,33 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 LiquidCrystal lcd(A5, A4, 2, 6, A3, A2);
-       
+                //RS  EN  D4 D5 D6  D7
 
 void setup()
 {
-    while (Ethernet.begin(mac) != 1)
-  {
-    Serial.println("Error getting IP address via DHCP, trying again...");
-    delay(15000);
-  }
+  
   sensors.begin();
   lcd.begin(16, 2);
-  
   pinMode(inputPin, INPUT);                      
   pinMode(REDLITE, OUTPUT);
   pinMode(BLUELITE, OUTPUT);
   digitalWrite(REDLITE,255);
   digitalWrite(BLUELITE,255);
-  
   Timer1.initialize(500000); 
-  
   emon1.current(sensorPin2, 111.1);
   emon2.current(sensorPin3, 111.1);
-    
+
+  Serial.begin(9600);
+  Serial.println("Starting single datastream upload to Xively...");
+  Serial.println();
+
+  while (Ethernet.begin(mac) != 1)
+  {
+    Serial.println("Error getting IP address via DHCP, trying again...");
+
+  }
+  
+  
 }
 
 void loop(void)
@@ -84,7 +91,7 @@ void loop(void)
     double Irms1 = emon1.calcIrms(1480); 
     double Irms2 = emon2.calcIrms(1480);  
  
-     
+    
   
 
   
@@ -116,8 +123,8 @@ void loop(void)
     if (digitalRead(inputPin) == LOW) {
     lcd.setCursor(10,1);
     lcd.print("      ");
-    }  
-
+    } 
+    
   } else {
     if (digitalRead(inputPin) == LOW){
       alarm=false;
@@ -133,7 +140,7 @@ void loop(void)
 
     }
     
-
+    // flash red
     Timer1.attachInterrupt(callback); 
   }
    
@@ -146,18 +153,16 @@ void loop(void)
   float sensorValue1 = analogRead(7);
   datastreams[0].setFloat(temp);
   
- 
   float sensorValue2 = analogRead(sensorPin2);
   datastreams[1].setFloat(Irms1);
   
-  float sensorValue3 = analogRead(sensorPin3);
-  datastreams[2].setFloat(Irms2);
   
   
-  if(Irms1<1) {
+  if(Irms1<0.5) {
       String stringValue(0);
       stringValue = "OFF";
       datastreams[3].setString(stringValue);
+      datastreams[1].setFloat(0);
   }
   else if(Irms1<3) {
         String stringValue(0);
@@ -169,24 +174,31 @@ void loop(void)
         stringValue = "Running";
         datastreams[3].setString(stringValue);
       }
-
-  if(Irms2<1) {
-      String stringValue(1);
+      
+  float sensorValue3 = analogRead(sensorPin3);
+  datastreams[2].setFloat(Irms2);
+  
+  {   
+  if(Irms2<0.5) {
+      String stringValue(0);
       stringValue = "OFF";
       datastreams[4].setString(stringValue);
+      datastreams[2].setFloat(0);
   }
-  else if(Irms2<10) {
-        String stringValue(1);
+  else if(Irms2<2) {
+        String stringValue(0);
         stringValue = "Water pump on";
         datastreams[4].setString(stringValue);
   }
   else  {
-        String stringValue(1);
+        String stringValue(0);
         stringValue = "Running";
         datastreams[4].setString(stringValue);
-      }  
-        int ret = xivelyclient.put(feed, xivelyKey);
-
+      }   
+          
+  }
+  int ret = xivelyclient.put(feed, xivelyKey);
+ 
 }
 
 
